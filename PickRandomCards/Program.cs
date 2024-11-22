6508758fcd03a30c08f8d86207a6f25126d76aab
asoto01 <mr.anthonygsoto@gmail.com>
2024-11-22 10:42:20 -0800

    Add project files.

---
+++ PickRandomCards/Program.cs
@@ -0,0 +1,29 @@
+﻿//// See https://aka.ms/new-console-template for more information
+//Console.WriteLine("Hello, World!");
+using System;
+
+namespace PickRandomCards 
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter the number of cards to pick: ");
+            string line = Console.ReadLine();
+            if (int.TryParse(line, out int numberOfCards))
+            {
+                string[] cards = CardPicker.pickSomeCards(numberOfCards);
+                foreach (string card in cards)
+                {
+                    Console.WriteLine(card);
+                }
+            } 
+            else
+            {
+                Console.WriteLine("Could not parse int; please enter an integer.");
+            }
+            
+
+        }
+    }
+}
